8ec13681aa25bd98e60a40a15463e2965207de7d
Michail Bondarenko <mcmikius15@gmail.com>
2019-03-07 12:59:11 +0200

    Controllers tests

---
+++ ToDoApp/ToDoAppTests/Controller/DataProviderTests.swift
@@ -0,0 +1,68 @@
+//
+//  DataProviderTests.swift
+//  ToDoAppTests
+//
+//  Created by Michail Bondarenko on 3/7/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import XCTest
+@testable import ToDoApp
+
+class DataProviderTests: XCTestCase {
+    
+    var sut: DataProvider!
+    var tableView: UITableView!
+
+    override func setUp() {
+        sut = DataProvider()
+        sut.taskManager = TaskManager()
+        tableView = UITableView()
+        tableView.dataSource = sut
+        
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func testNumberOfSectionsIsTwo() {
+        
+        let numberOfSections = tableView.numberOfSections
+        
+        XCTAssertEqual(numberOfSections, 2)
+    }
+    
+    func testNumberOfRowsInSectionZeroIsTasksCount() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 1)
+        
+        sut.taskManager?.add(task: Task(title: "Bar"))
+        tableView.reloadData()
+        XCTAssertEqual(tableView.numberOfRows(inSection: 0), 2)
+    }
+    
+    func testNumberOfRowsInSectionOneIsDoneTasksCount() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        sut.taskManager?.checkTask(at: 0)
+        
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 1)
+        
+        sut.taskManager?.add(task: Task(title: "Bar"))
+        sut.taskManager?.checkTask(at: 0)
+        tableView.reloadData()
+        XCTAssertEqual(tableView.numberOfRows(inSection: 1), 2)
+    }
+    
+    func testCellForRowAtIndexPathReturnsTaskCell() {
+        sut.taskManager?.add(task: Task(title: "Foo"))
+        tableView.reloadData()
+        
+        let cell = tableView.cellForRow(at: IndexPath(row: 0, section: 0))
+        XCTAssertTrue(cell is TaskCell)
+    }
+    
+
+
+}
